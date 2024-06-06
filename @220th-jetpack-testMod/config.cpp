599e46f346de9cc5e33ab229dafb2ebb9acfe3ff
Tally-1 <HartgenLeo@gmail.com>
2024-06-06 09:55:08 -0300

    Testmod

---
+++ @220th-jetpack-testMod/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches
+{
+    class Shops_220
+    {
+        
+        name             = "220-th Modules";
+        author           = "[P] Tally";
+        requiredVersion  = 1.60;
+        requiredAddons[] = {};
+        units[]          = {};
+        weapons[]        = {};
+    };
+};
---
+++ @220th-jetpack-testMod/mod.cpp
@@ -0,0 +1,15 @@
+ author 		= "[P] Tally";         // Affects Arma 3 Launcher, when the mod are loaded as local
+ name 			= "220 Jetpack tester";      // Name of your mod
+ tooltip 		= "220 Jetpack tester";
+ tooltipOwned 	= "220 Jetpack tester";      // Tool tip displayed when the mouse is left over, in the main menu
+ actionName 	= "";                  // Button-text
+ action 		= "";                  // Website URL, that can be accessed from the expansions menu
+ overview 		= "";                  // Supports structured text
+ hideName 		= 0;                   // Hide the extension name
+ hidePicture	= 0;	               // Hide the extension menu
+ dlcColor[] 	= {0.23,0.39,0.30,1};  // Color used for DLC stripes and backgrounds (RGBA)
+
+ picture 		= "[P].paa";    // Picture displayed from the expansions menu. Optimal size is 2048x1024
+ logoSmall 		= "[P].paa";   // Display in creative lists, next to the entities added by the mod
+ logo 			= "[P].paa";  // Logo displayed in the main menu
+ logoOver 		= "[P].paa"; // When the mouse is over, in the main menu
